b31e8270f5bcea00e82c5f3cbb7a4fcf8131aec4
keyzhaus <98558356+keyzhaus@users.noreply.github.com>
2022-01-27 14:40:18 -0800

    LG steez

--- config.js
+++ config.js
@@ -1,41 +1,42 @@
 const CONFIG = {
-  hour12  : false
+  hour12  : true
 }
 
 const ICONS = [
   {
-    icon: "ri-youtube-fill",
-    link: "https://www.youtube.com/"
+    icon: "ri-dashboard-3-line",
+    link: "https://business.facebook.com/adsmanager/reporting/business_view?act=643842749473651&business_id=982768378465691&selected_report_id=3497261477016356"
   },
   {
-    icon: "ri-whatsapp-fill",
-    link: "https://web.whatsapp.com/"
+    icon: "ri-instagram-line",
+    link: "https://www.instagram.com"
   },
   {
-    icon: "ri-reddit-fill",
-    link: "https://www.reddit.com/"
+    icon: "ri-dropbox-fill",
+    link: "https://www.dropbox.com/"
   },
   {
-    icon: "ri-github-fill",
-    link: "https://www.github.com/"
-  }
+    icon: "ri-oil-fill",
+    link: "https://www.vitalityextracts.com/"
+  },
 ]
 
 const LIST_ITEMS = [
   {
-    name: "music",
-    link: "https://music.youtube.com"
+    name: "RA",
+    link: "https://business.facebook.com/adsmanager/manage/campaigns?act=1334057436670115&business_id=982768378465691&global_scope_id=982768378465691&columns=name%2Ccampaign_group_name%2Ccampaign_name%2Cschedule%2Cdelivery%2Cbudget%2Cbid%2Cspend%2Cactions%3Alink_click%2Ccost_per_action_type%3Alink_click%2Cwebsite_ctr%3Alink_click%2Cactions%3Aoffsite_conversion.fb_pixel_add_to_cart%2Ccost_per_action_type%3Aomni_add_to_cart%2Cactions%3Aoffsite_conversion.fb_pixel_purchase%2Ccost_per_action_type%3Aomni_purchase%2Cwebsite_purchase_roas%3Aoffsite_conversion.fb_pixel_purchase%2Caction_values%3Aoffsite_conversion.fb_pixel_purchase%2Ccustom_derived_metrics%3A10221704125486547%2Cactions%3Aomni_view_content%2Ccost_per_action_type%3Aomni_view_content%2Creach%2Cfrequency%2Cimpressions%2Ccpm%2Cactions%3Aomni_initiated_checkout%2Cactions%3Aadd_payment_info%2Ccost_per_action_type%3Aadd_payment_info%2Ccost_per_action_type%3Aomni_initiated_checkout%2Crelevance_score%3Ascore%2Cquality_score_organic%2Cquality_score_ectr%2Cquality_score_ecvr%2Cactions%3Aoffsite_conversion.custom.236621274124375%2Cattribution_setting%2Cactions%3Aonsite_conversion.purchase%2Cactions%3Aapp_custom_event.fb_mobile_purchase&attribution_windows=default&date=2021-09-23_2021-09-24%2Ctoday&insights_date=2021-09-23_2021-09-24%2Ctoday&filter_set=CAMPAIGN_DELIVERY_STATUS-STRING_SET%1EIN%1E[%22active%22%2C%22deleted%22%2C%22error%22%2C%22inactive%22%2C%22off%22%2C%22pending%22]%1Dadset.impressions-NUMBER%1EGREATER_THAN%1E0&impression_breakdown=impression"
+  },
+    {
+    name: "SA",
+    link: "https://business.facebook.com/adsmanager/manage/campaigns?act=414290980023670&business_id=982768378465691&global_scope_id=982768378465691&date=2021-04-22_2021-04-23%2Ctoday&comparison_date="
   },
   {
-    name: "netflix",
-    link: "https://www.netflix.com/"
+    name: "TT",
+    link: "https://business.tiktok.com/manage/member?org_id=7057591459096035329"
   },
+  
   {
-    name: "r/unixporn",
-    link: "https://www.reddit.com/r/unixporn/"
-  },
-  {
-    name: "r/firefoxcss",
-    link: "https://www.reddit.com/r/firefoxcss/"
+    name: "zip",
+    link: "https://zipifypages.zipify.com/?hmac=307b5e6418f95320caf61dfb8ed69bc08fabe5bcc06f72c5e11ca265b7f622b5&session=a29557bd32e8933c058c75c12a2f844db922e208ebfc70a23a5007431f7ee8be&shop=vitality-extracts.myshopify.com&timestamp=1586802346"
   },
 ]
